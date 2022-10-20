a3d7f38974d61d30cdc2225f56856ca0fac4a75e
Madahva <galarzaguillermo.ggm@gmail.com>
2022-10-20 10:31:35 -0300

    Add Exercise--3

---
+++ Ejercicio--3/src/com/exercise/Bucles.java
@@ -0,0 +1,14 @@
+package com.exercise;
+
+public class Bucles {
+    public static void main(String[] args) {
+        String[] textoSeparado = {"Soy", "un", "texto", "corto."};
+        String textoNuevo = "";
+
+        for (String texto : textoSeparado) {
+            textoNuevo += texto + " ";
+        }
+
+        System.out.println(textoNuevo);
+    }
+}
