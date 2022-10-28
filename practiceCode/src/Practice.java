b12467b997883814df52596760fcb72203a0a2f2
Madahva <galarzaguillermo.ggm@gmail.com>
2022-10-27 21:52:57 -0300

    Add Exercise--5

--- practiceCode/src/Practice.java
+++ practiceCode/src/Practice.java
@@ -1,24 +1,29 @@
+import java.util.Arrays;
+
 public class Practice {
     public static void main(String[] args) {
+        int[] statues = {6, 2, 3, 8};
+        System.out.println(solution(statues));
 
-        int[] inputArray = {-22222223, 6, -2, -5, 7, 3};
-        solution(inputArray);
     }
 
-    public static int solution(int[] inputArray) {
+    static int[] solution(int[] statues) {
+            Arrays.sort(statues);
 
-        int actProduct = 0;
-        int maxProduct = -1000;
+        int result = 0;
 
-        for (int i = 0; i < inputArray.length -1; i++) {
+        for (int i = 0; i < statues.length; i++) {
 
-            actProduct = inputArray[i] * inputArray[i+1];
+            int diff = statues[i] - statues[i+1];
 
-            if (maxProduct < actProduct) {
-                maxProduct = actProduct;
-            }
+            if (diff != 1) result++;
         }
-        System.out.println(maxProduct);
-        return maxProduct;
-    }
+
+
+        return statues;
+        }
+
+
+// [2,3,6,8]
+
 }
